2df8ba49c1bd1f000bcde47e0c1e6d76b9832526
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-05-11 16:47:33 -0500

    delete router working and is require to pass the password in order to work

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ router.get('/login', async (req,res)=>{
 		const foundUser = await User.findOne({username:req.body.username});
 		
 		if (foundUser){
-			console.log(foundUser);
+			// campare the password pass into the req.body.password match with the hash password
 			if(bcrypt.compareSync(req.body.password, foundUser.password)){
 
 				res.send(foundUser)
@@ -23,7 +23,7 @@ router.get('/login', async (req,res)=>{
 	}
 });
 
-router.post('/register', async (req,res)=>{
+router.post('/register', async (req,res, next)=>{
 	try{
 
 		const newUser = req.body;
@@ -36,8 +36,35 @@ router.post('/register', async (req,res)=>{
 		res.send(createUser)
 	}
 	catch{
-
+		next(err)
 	}
+});
+
+router.delete('/:id', async (req,res,next)=>{
+	try{
+		console.log('hit the delete router');
+		console.log('req.params.id', req.params.id);
+		const userToBeRemoved = await User.findById(req.params.id);
+		
+		if (userToBeRemoved){
+			console.log('foundUser to be removed');
+			// campare the password pass into the req.body.password match with the hash password
+			if(bcrypt.compareSync(req.body.password, userToBeRemoved.password)){
+				console.log(' same password');
+				const removedUser = await User.findByIdAndRemove(userToBeRemoved._id)
+
+				res.send(removedUser)
+			}
+			else{
+				console.log('not the same password');
+				res.send('NOPE NOPE Delete Router')
+			}
+		}
+	}
+	catch{
+		console.log('oppps something wrong');
+		next(err)
+	}	
 });
 
 module.exports = router
